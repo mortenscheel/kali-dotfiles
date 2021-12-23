0cd43fb38bd458f4a59a9b71735c6422cb966d16
Morten Scheel <morten@mortenscheel.com>
2021-12-23 20:52:25 +0100

    update files.zsh

--- .zsh/files.zsh
+++ .zsh/files.zsh
@@ -1,6 +1,8 @@
 export php_revshell="/usr/share/webshells/php/php-reverse-shell.php"
 export rockyou="/usr/share/wordlists/rockyou.txt"
-export dirlist_sm="/usr/share/wordlists/dirbuster/directory-list-2.3-small.txt"
-export dirlist_md="/usr/share/wordlists/dirbuster/directory-list-lowercase-2.3-medium.txt"
+export seclists=/usr/share/seclists
+export web_content="$seclists/Discovery/Web-Content"
+export dirlist_sm="$web_content/directory-list-lowercase-2.3-small.txt"
+export dirlist_md="$web_content/directory-list-lowercase-2.3-medium.txt"
 export common="/usr/share/seclists/Discovery/Web-Content/common.txt"
 export linpeas="/opt/linpeas.sh"
