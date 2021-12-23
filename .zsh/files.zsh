884808f0fa339c592425a98bea960a30a9290d10
Morten Scheel <morten@mortenscheel.com>
2021-12-23 20:59:23 +0100

    add nmap

--- .zsh/files.zsh
+++ .zsh/files.zsh
@@ -6,3 +6,4 @@ export dirlist_sm="$web_content/directory-list-lowercase-2.3-small.txt"
 export dirlist_md="$web_content/directory-list-lowercase-2.3-medium.txt"
 export common="/usr/share/seclists/Discovery/Web-Content/common.txt"
 export linpeas="/opt/linpeas.sh"
+export nmap=/usr/share/nmap
