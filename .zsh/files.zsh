ac849431c167e6b340a570b1a3e1a2ce25e8fa94
Morten Scheel <morten@mortenscheel.com>
2022-04-13 17:46:43 +0200

    update

--- .zsh/files.zsh
+++ .zsh/files.zsh
@@ -5,5 +5,5 @@ export web_content="$seclists/Discovery/Web-Content"
 export dirlist_sm="$web_content/directory-list-lowercase-2.3-small.txt"
 export dirlist_md="$web_content/directory-list-lowercase-2.3-medium.txt"
 export common="/usr/share/seclists/Discovery/Web-Content/common.txt"
-export linpeas="/opt/linpeas.sh"
+export linpeas="~/tools/PEASS-ng/linPEAS/linpeas.sh"
 export nmap=/usr/share/nmap
