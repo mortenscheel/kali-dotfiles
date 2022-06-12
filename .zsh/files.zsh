7fb5bba1c2fe6215c83151ed770a36960ff37c27
Morten Scheel <morten@mortenscheel.com>
2022-06-12 11:18:29 +0200

    update

--- .zsh/files.zsh
+++ .zsh/files.zsh
@@ -7,3 +7,4 @@ export dirlist_md="$web_content/directory-list-lowercase-2.3-medium.txt"
 export common="/usr/share/seclists/Discovery/Web-Content/common.txt"
 export linpeas="~/tools/PEASS-ng/linPEAS/linpeas.sh"
 export nmap=/usr/share/nmap
+export fasttrack=/usr/share/wordlists/fasttrack.txt
